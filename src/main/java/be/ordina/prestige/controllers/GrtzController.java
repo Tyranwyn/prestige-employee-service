8842ba8be324a4a08d36adb862164d8ea9e9c6fa
Fux <Sammi.Fux@ordina.be>
2017-02-22 14:20:43 +0100

    basic swagger api

---
+++ src/main/java/be/ordina/prestige/controllers/GrtzController.java
@@ -0,0 +1,7 @@
+package be.ordina.prestige.controllers;
+
+/**
+ * Created by SaFu on 22/02/2017.
+ */
+public class GrtzController {
+}
---
+++ src/main/java/be/ordina/prestige/model/Grtz.java
@@ -0,0 +1,7 @@
+package be.ordina.prestige.model;
+
+/**
+ * Created by SaFu on 22/02/2017.
+ */
+public class Grtz {
+}
