867ad6b10b788b62f7b94abe58615872547c60e0
Fux <Sammi.Fux@ordina.be>
2017-02-22 14:33:20 +0100

    Basic Swagger Rest API Implementation

--- src/main/java/be/ordina/prestige/controllers/GrtzController.java
+++ src/main/java/be/ordina/prestige/controllers/GrtzController.java
@@ -1,7 +1,38 @@
 package be.ordina.prestige.controllers;
 
+import be.ordina.prestige.model.Grtz;
+import io.swagger.annotations.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * Created by SaFu on 22/02/2017.
  */
+@RestController
 public class GrtzController {
+
+    private static final String template = "Greetings u little, %s";
+    private final AtomicLong counter = new AtomicLong();
+
+    @ApiOperation(value = "getGreeting", nickname = "getGreeting")
+    @RequestMapping(method = RequestMethod.GET, path = "/grtz", produces = "application/json")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "name", value = "User's name", required = false, dataType = "string", paramType = "query", defaultValue = "Dildo"),
+            @ApiImplicitParam(name = "lastname", value = "User's last name", required = false, dataType = "string", paramType = "query", defaultValue = "Swaggins")
+    })
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Poortje is open ;)", response = Grtz.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Failure")
+    })
+    public Grtz grtz(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Grtz(counter.incrementAndGet(), String.format(template, name));
+    }
+
 }
--- src/main/java/be/ordina/prestige/model/Grtz.java
+++ src/main/java/be/ordina/prestige/model/Grtz.java
@@ -1,7 +1,28 @@
 package be.ordina.prestige.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * Created by SaFu on 22/02/2017.
  */
 public class Grtz {
+
+    private final long id;
+    private final String content;
+
+    public Grtz(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    @JsonProperty(required = true)
+    @ApiModelProperty(notes = "The name of the user", required = true)
+    public String getContent() {
+        return content;
+    }
 }
