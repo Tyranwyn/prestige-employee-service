2ca67db16d81a05cee02a86ee7df39dde63d6547
Fux <Sammi.Fux@ordina.be>
2017-02-22 13:23:47 +0100

    Initializing project

---
+++ src/main/java/be/ordina/prestige/PrestigeEmployeeServiceApplication.java
@@ -0,0 +1,12 @@
+package be.ordina.prestige;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestigeEmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestigeEmployeeServiceApplication.class, args);
+	}
+}
